9de3a7740a0b75ef68ffb98664c51911ae43e086
Nick East <nickeast1998@gmail.com>
2024-06-27 08:41:45 +0100

    latest

--- src/html.js
+++ src/html.js
@@ -13,17 +13,38 @@ export default function HTML(props) {
         />
         <link rel="stylesheet" href="https://use.typekit.net/ltj6ded.css" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-BDTKHNEGQK"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() { dataLayer.push(arguments); }
-            gtag('js', new Date());
-            gtag('config', 'G-BDTKHNEGQK');
-          `}
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BDTKHNEGQK');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MLB474RM');
+            `,
+          }}
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MLB474RM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {props.preBodyComponents}
         <div
           key={`body`}
